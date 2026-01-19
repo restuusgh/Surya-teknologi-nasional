2dc3dc1a6c2255de8a71ebf89bba26af53786c86
Anisanurulainii <anisaedoh@gmail>
2026-01-19 14:07:31 +0700

    resolve conflict Admin model

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -1,47 +1,40 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
-const Admin = sequelize.define("Admin", {
-<<<<<<< HEAD
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true, // username harus unik
+const Admin = sequelize.define(
+  "Admin",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,   // AMAN untuk data lama
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-=======
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-  },
-  lastName: {
-    type: DataTypes.STRING,
->>>>>>> 3211f147c6e7bd58024ba3e6d9dd233306592635
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default Admin;
